02e7223f885ed0241ba1d2edd8333c6e6154e3cb
Lukas Berghegger <lukasberghegger@gmail.com>
2025-12-03 14:46:14 +0100

    LB: went back to prefab management, finally fixed baking by lowering lightmap resolution

--- Assets/Scripts/Gameplay/SceneLoader.cs
+++ Assets/Scripts/Gameplay/SceneLoader.cs
@@ -1,35 +1,47 @@
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace Gameplay
 {
+    [ExecuteInEditMode]
     public class SceneLoader : MonoBehaviour
     {
-        public string scenePath;
+        public GameObject scenePrefab;
         public Vector2Int grid;
         public float gridSize = 100;
 
-        private bool _dirty;
+        readonly List<GameObject> _instances = new();
+        bool _dirty;
 
-        private void Update()
+        void Update()
         {
             if (!_dirty)
                 return;
 
-            for (var i = 0; i < grid.x * grid.y; i++) SceneManager.LoadScene(scenePath, LoadSceneMode.Additive);
+            for (var i = transform.childCount - 1; i >= 0; i--)
+            {
+                var go = transform.GetChild(i).gameObject;
+                _instances.Remove(go);
+                DestroyImmediate(go);
+            }
 
+            _instances.Clear();
             for (var x = 0; x < grid.x; x++)
             for (var y = 0; y < grid.y; y++)
             {
-                var gos = SceneManager.GetSceneAt(x + x * y).GetRootGameObjects();
-                foreach (var go in gos)
-                    go.transform.localPosition = new Vector3(x * gridSize, 0, y * gridSize);
+                var scene = PrefabUtility.InstantiatePrefab(scenePrefab, transform) as GameObject;
+                if (!scene)
+                    continue;
+
+                scene.transform.position = new Vector3(x, 0, y) * gridSize;
+                _instances.Add(scene);
             }
 
             _dirty = false;
         }
 
-        private void OnValidate()
+        void OnValidate()
         {
             _dirty = true;
         }
