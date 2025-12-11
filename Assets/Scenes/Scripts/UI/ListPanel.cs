452026a1d48d28b958b085f00dcf297532135803
Ubus99 <36008694+Ubus99@users.noreply.github.com>
2025-12-11 21:12:14 +0100

    LB: updated POV screen

---
+++ Assets/Scenes/Scripts/UI/ListPanel.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using UnityEngine;
+
+namespace Scenes.Scripts.UI
+{
+    [ExecuteAlways]
+    public class ListPanel : MonoBehaviour
+    {
+        public string title;
+        public bool leftIcon;
+        public bool rightIcon;
+
+        public TextMeshProUGUI titleText;
+        public GameObject buttonLeft;
+        public GameObject buttonRight;
+
+        void OnValidate()
+        {
+            if (titleText) titleText.text = title;
+            if (buttonLeft) buttonLeft.SetActive(leftIcon);
+            if (buttonRight) buttonRight.SetActive(rightIcon);
+        }
+    }
+}
