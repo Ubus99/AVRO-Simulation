2b59aec9210db09d8b8da451d05bff6857ee2e62
Ubus99 <36008694+Ubus99@users.noreply.github.com>
2025-12-12 13:22:19 +0100

    LB: added extra functionality to list items

--- Assets/Scenes/Scripts/UI/ListItem.cs
+++ Assets/Scenes/Scripts/UI/ListItem.cs
@@ -1,14 +1,64 @@
-using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scenes.Scripts.UI
 {
     public class ListItem : MonoBehaviour
     {
+        [Header("References")]
         public GameObject leftButton;
+
         public GameObject rightButton;
+
         public TextMeshProUGUI title;
+
         public TextMeshProUGUI label;
+
+        [Header("Options")]
+        public bool selectable = true;
+
+        public bool showLeftButton;
+
+        public bool showRightButton;
+
+        public bool showLabel;
+
+        public string titleText;
+
+        public string labelText;
+
+        //privates
+        Button _button;
+
+        void Awake()
+        {
+            RefreshComponents();
+        }
+
+        void OnValidate()
+        {
+            RefreshComponents();
+
+            _button.interactable = selectable;
+            if (leftButton) leftButton.SetActive(showLeftButton);
+            if (rightButton) rightButton.SetActive(showRightButton);
+            if (title) title.text = titleText;
+            if (label)
+            {
+                label.gameObject.SetActive(showLabel);
+                label.text = labelText;
+            }
+        }
+
+        void RefreshComponents()
+        {
+            _button = GetComponent<Button>();
+        }
+
+        public void ToggleSelectable()
+        {
+            selectable = !selectable;
+        }
     }
 }
