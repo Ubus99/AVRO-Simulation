d0ed9f8c71199b594a2bdd5138ea0d2182edb3ce
Ubus99 <36008694+Ubus99@users.noreply.github.com>
2025-12-19 02:04:12 +0100

    LB: added weighted tabole, untested, proceeding with car tests

--- Assets/Scripts/Streets/JunctionTrigger.cs
+++ Assets/Scripts/Streets/JunctionTrigger.cs
@@ -1,11 +1,23 @@
-using AYellowpaper.SerializedCollections;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Splines;
+using Utils;
 
 namespace Streets
 {
     public class JunctionTrigger : MonoBehaviour
     {
-        public SerializedDictionary<SplineContainer, float> junctionData = new();
+        public WeightedPool<SplineContainer> junctionData = new();
+
+        public bool TryGetRandomExit(out SplineContainer exit)
+        {
+            if (!junctionData.Any(pair => pair.Value > 0))
+            {
+                exit = null;
+                return false;
+            }
+            exit = junctionData.DrawRandom();
+            return true;
+        }
     }
 }
---
+++ Assets/Scripts/Utils/WeightedPool.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using AYellowpaper.SerializedCollections;
+using Random = UnityEngine.Random;
+
+namespace Utils
+{
+    [Serializable]
+    public class WeightedPool<T> : SerializedDictionary<T, float>
+    {
+        public T DrawRandom()
+        {
+            var available = this
+                .Where(pair => pair.Value > 0)
+                .OrderBy(pair => pair.Value)
+                .ToList();
+
+            var range = available.Sum(pair => pair.Value);
+            var i = Random.value * range;
+
+            float acc = 0;
+            foreach (var kvp in available)
+            {
+                if (kvp.Value + acc < i)
+                {
+                    return kvp.Key;
+                }
+                acc += kvp.Value;
+            }
+            return available.First().Key;
+        }
+    }
+}
