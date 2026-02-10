21ce980e5d5ea5eca5fa1eaa85d03a6ad968d942
Ubus99 <36008694+Ubus99@users.noreply.github.com>
2026-02-10 10:23:52 +0100

    LB: logger working correctly

--- Assets/Scripts/Utils/CSVLogger.cs
+++ Assets/Scripts/Utils/CSVLogger.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using CsvHelper;
-using Unity.Cecil.Awesome.Ordering;
 using UnityEngine;
 using Utils.Objects;
 
@@ -13,7 +12,7 @@ namespace Utils
 {
     public class CSVLogger : MonoBehaviour
     {
-        readonly List<Dictionary<string, string>> _data = new();
+        readonly DataTable _data = new() { Columns = { "timestamp" } };
         readonly Dictionary<string, string> _frameData = new();
         bool _empty;
 
@@ -33,9 +32,10 @@ namespace Utils
             if (_empty) return;
 
             // ensure only logs when there is data
-            _frameData["timestamp"] = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss:zzz");
-            _data.Add(new Dictionary<string, string>(_frameData));
-            foreach (var k in _frameData.Keys.ToList()) // remove old data
+            _frameData["timestamp"] = DateTime.Now.ToString("HH:mm:ss:ffff");
+            JoinDataTable(_data, _frameData);
+            // remove old data
+            foreach (var k in _frameData.Keys.ToList())
             {
                 _frameData[k] = null;
             }
@@ -44,20 +44,32 @@ namespace Utils
             using var writer = new StreamWriter(_filePath);
             using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
 
-            foreach (var stream in _data[0])
+            var columnNames = _data.Columns.Cast<DataColumn>().Select(column => column.ColumnName).ToArray();
+            foreach (var colName in columnNames)
             {
-                csv.WriteField(stream.Key);
+                csv.WriteField(colName);
             }
             csv.NextRecord();
 
-            foreach (var record in _data)
+            foreach (DataRow row in _data.Rows)
             {
-                foreach (var stream in record)
+                var fields = row.ItemArray.Select(field => field.ToString());
+                foreach (var field in fields)
                 {
-                    csv.WriteField(stream.Value);
+                    csv.WriteField(field);
                 }
                 csv.NextRecord();
             }
+        }
+
+        static void JoinDataTable(DataTable table, Dictionary<string, string> dictionary)
+        {
+            var row = table.NewRow();
+            foreach (var kvp in dictionary)
+            {
+                row[kvp.Key] = kvp.Value;
+            }
+            table.Rows.Add(row);
         }
 
         public void RestartLogging(string newName)
@@ -66,15 +78,19 @@ namespace Utils
             _data.Clear();
             RegistrationEvent?.Invoke();
         }
-        
+
         public bool TryRegister(string key)
         {
-            return _frameData.TryAdd(key, null);
+            if (!_frameData.TryAdd(key, null))
+                return false;
+
+            _data.Columns.Add(key, typeof(string));
+            return true;
         }
 
         public bool TryRegister(string[] keys)
         {
-            return keys.Select(key => _frameData.TryAdd(key, null)).All(success => success);
+            return keys.All(key => _frameData.TryAdd(key, null));
         }
 
         public bool TryLog(string key, string message)
